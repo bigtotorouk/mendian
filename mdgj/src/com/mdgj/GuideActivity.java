3090a7642554d2943648bc2f654b3455feb42439
xiaobing <bigtotorouk@gmail.com>
2014-06-25 11:27:45 +0800

    代码整合

---
+++ mdgj/src/com/mdgj/GuideActivity.java
@@ -0,0 +1,7 @@
+package com.mdgj;
+
+import android.support.v4.app.FragmentActivity;
+
+public class GuideActivity extends FragmentActivity {
+	
+}
---
+++ mdgj/src/com/mdgj/fragment/guide/TestFragmentAdapter.java
@@ -0,0 +1,51 @@
+package com.mdgj.fragment.guide;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.mdgj.R;
+import com.viewpagerindicator.IconPagerAdapter;
+
+class TestFragmentAdapter extends FragmentPagerAdapter implements IconPagerAdapter {
+    protected static final String[] CONTENT = new String[] { "This", "Is", "A", "Test", };
+    protected static final int[] ICONS = new int[] {
+            R.drawable.guide0,
+            R.drawable.guide1,
+            R.drawable.guide2,
+            R.drawable.guide3
+    };
+
+    private int mCount = CONTENT.length;
+
+    public TestFragmentAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return TestFragment.newInstance(CONTENT[position % CONTENT.length]);
+    }
+
+    @Override
+    public int getCount() {
+        return mCount;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+      return TestFragmentAdapter.CONTENT[position % CONTENT.length];
+    }
+
+    @Override
+    public int getIconResId(int index) {
+      return ICONS[index % ICONS.length];
+    }
+
+    public void setCount(int count) {
+        if (count > 0 && count <= 10) {
+            mCount = count;
+            notifyDataSetChanged();
+        }
+    }
+}
